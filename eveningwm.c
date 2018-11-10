a215753c402953ceb40af783149e224219a8e93b
Mr messyhair <kristo.ilmari@gmail.com>
2018-11-10 08:14:22 +0000

    Update eveningwm.c

--- eveningwm.c
+++ eveningwm.c
@@ -10,10 +10,12 @@ int main(void)
 {
     Display * dpy;
     XWindowAttributes attr;
+    static XSetWindowAttributes xattr;
     XButtonEvent start;
     XEvent ev; 
-    char term[] = "urxvt &";
-
+    Window foc;
+    int revert_to;
+   
     /*If we can't open display return 1*/    
     if(!(dpy = XOpenDisplay(0x0))) return 1; 
 	
@@ -33,45 +35,54 @@ int main(void)
     XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
+    xattr.event_mask = SubstructureNotifyMask;
+    XChangeWindowAttributes(dpy, DefaultRootWindow(dpy), CWEventMask, &xattr);
     start.subwindow = None;
-   
+    
     for(;;)
     {
-        XNextEvent(dpy, &ev);
-	/*Use xev to get keycodes if you want to modify these*/
-	switch(ev.xkey.keycode)
+            XNextEvent(dpy, &ev);
+	        XGetInputFocus(dpy, &foc, &revert_to);
+
+	if(ev.type == KeyPress && ev.xbutton.subwindow != None)
 	{
-		case 36:	
-			system(term);
-		case 19:
-			XCloseDisplay(dpy);
-		case 24:
-			XDestroyWindow(dpy, ev.xbutton.subwindow);	
-		case 40:
-			XLowerWindow(dpy, ev.xbutton.subwindow);
-		case 27:
-			XRaiseWindow(dpy, ev.xbutton.subwindow);
+		    if(ev.xkey.keycode == 24)
+		    	XDestroyWindow(dpy, ev.xbutton.subwindow);	
+		    else if (ev.xkey.keycode == 40)
+		    	XLowerWindow(dpy, ev.xbutton.subwindow);
+		    else if (ev.xkey.keycode == 27)
+		    	XRaiseWindow(dpy, ev.xbutton.subwindow);
 	}
 
-        if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
-        {
-	    /*Save the windows size and location before doing anything*/
-            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-            start = ev.xbutton;
-        }
-        else if(ev.type == MotionNotify && start.subwindow != None)
-        {
+	if(ev.type == KeyPress)
+	{
+	    	if(ev.xkey.keycode == 36)
+		    	system("urxvt &");
+	    	else if(ev.xkey.keycode == 19)
+		    	XCloseDisplay(dpy);
+	}
+
+	if(ev.type == ButtonPress && ev.xbutton.subwindow != None) 
+    {
+	        /*Save the windows size and location before doing anything*/ 
+	        XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+	        XSetInputFocus(dpy, ev.xbutton.subwindow, RevertToParent, CurrentTime); 
+            start = ev.xbutton;  
+	    
+    }
+    else if(ev.type == MotionNotify && start.subwindow != None)
+    {
             int xdiff = ev.xbutton.x_root - start.x_root;
             int ydiff = ev.xbutton.y_root - start.y_root;
             XMoveResizeWindow(dpy, start.subwindow,
                 attr.x + (start.button==1 ? xdiff : 0),
                 attr.y + (start.button==1 ? ydiff : 0),
-		/*Minimum window size when resizing is 100x50 pixels because
-		 *I don't think there is any need for smaller windows*/
+		        /*Minimum window size when resizing is 100x50 pixels because
+		        *I don't think there is any need for smaller windows*/
                 MAX(100, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(50, attr.height + (start.button==3 ? ydiff : 0)));
-        }
+    }
         else if(ev.type == ButtonRelease)
-            start.subwindow = None;
+                start.subwindow = None;
     }
 }
